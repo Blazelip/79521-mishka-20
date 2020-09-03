b351eb62a4b1820140f8c7658fc8ae0f42b003ec
Blazelip <filin1994@rambler.ru>
2020-09-03 23:39:32 +0300

    JS для модалки и меню + правки по ПП для мобильных версий страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,34 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+document.addEventListener("DOMContentLoaded", function() {
 
-navMain.classList.remove('main-nav--nojs');
+  var navMain = document.querySelector(".main-nav");
+  var navToggle = navMain.querySelector(".main-nav__toggle");
+  var openCart = document.querySelectorAll(".open-cart");
+  var popup = document.querySelector(".modal-cart");
+  var closeCart = popup.querySelector(".close-cart");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+  navMain.classList.remove("main-nav--nojs");
+
+  navToggle.addEventListener("click", function() {
+    navMain.classList.toggle("main-nav--closed");
+    navMain.classList.toggle("main-nav--opened");
+  });
+
+  for (i = 0; i < openCart.length; i++) {
+    openCart[i].addEventListener("click", function(evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-cart--show");
+    });
   }
+
+  closeCart.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-cart--show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27 && popup.classList.contains("modal-cart--show")) {
+        evt.preventDefault();
+        popup.classList.remove("modal-cart--show");
+    }
+  });
 });
